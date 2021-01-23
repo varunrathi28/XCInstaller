d013499631b546e52197b5cd5610e157b1c24098
Varun Rathi <varunrathi28@users.noreply.github.com>
2021-01-23 13:56:00 +0530

    Device Data store Logic: 1. Fetching device list 2. Saving device list 3. Checking if device already exists

---
+++ XCInstaller/DataStore/DeviceDataStore.swift
@@ -0,0 +1,86 @@
+//
+//  DeviceDataStore.swift
+//  XCInstaller
+//
+//  Created by VR on 23/01/21.
+//  Copyright © 2021 VR. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import CoreData
+
+
+class DeviceDataStore {
+    
+    let deviceEntityName = "Device"
+    var mainContext: NSManagedObjectContext =  {
+        let delegate = NSApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        return context
+    }()
+    
+    func getSimulatorDevicesList() -> [Device]? {
+        let fetchRequest: NSFetchRequest<Device> = Device.fetchRequest()
+        do {
+            if let result:[Device] = try mainContext.fetch(fetchRequest), !result.isEmpty {
+                return result
+            }
+        }
+        catch{
+            print("Failed to fetch device list")
+        }
+        return nil
+    }
+    
+    func saveSimulatorListToStore( devices : [[String:AnyObject]]) {
+        guard devices.isEmpty == false else { return }
+        mainContext.perform {
+            devices.forEach { dic in
+                guard let udid = dic["udid"] as? String else { return }
+                if !self.checkIfDeviceExists(for: udid, in: self.mainContext) {
+                     self.insertDeviceEntity(dic, in: self.mainContext)
+                }
+            }
+
+            do {
+                try self.mainContext.save()
+            }
+            catch {
+                print("Unnable to save device")
+            }
+        }
+    }
+    
+    func insertDeviceEntity(_ dic: [String:AnyObject],  in context: NSManagedObjectContext) {
+         guard let udid = dic["udid"] as? String else { return }
+        
+        if let deviceEntity = NSEntityDescription.entity(forEntityName: deviceEntityName, in: context) {
+            if  let  deviceObj = NSManagedObject(entity: deviceEntity, insertInto: context) as? Device {
+        
+                deviceObj.udid = udid
+                deviceObj.name = dic["name"] as? String
+                deviceObj.available = dic["available"] as? Bool ?? false
+                deviceObj.state = dic["state"] as? String
+                
+            }
+        }
+    }
+    
+    func checkIfDeviceExists(for udid:String, in context: NSManagedObjectContext) -> Bool {
+           let fetchRequest: NSFetchRequest<Device> = Device.fetchRequest()
+           fetchRequest.predicate = NSPredicate(format: "%K = %@","udid",udid)
+           fetchRequest.fetchLimit = 1
+           do {
+               if let result = try context.fetch(fetchRequest) as? [Device], result .isEmpty == false{
+                   return true
+               }
+           }
+           catch {
+               print("Device Fetch failed")
+           }
+          return false
+       }
+       
+
+}
